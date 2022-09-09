c3352eb43d84806ee7d583470db84215d8d4229b
kassimu21 <sksallah45@gmail.com>
2022-09-09 14:04:19 -0700

    My nineth if else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Description: prints base 16 numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

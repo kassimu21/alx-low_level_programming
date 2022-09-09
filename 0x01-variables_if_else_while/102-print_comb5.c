8ebd272dcbc76ce61dcb446584fdf80619a07187
kassimu21 <sksallah45@gmail.com>
2022-09-09 15:14:29 -0700

    Else if while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,26 @@ int main(void)
 	{
 		for (i = 48; i <= 57; i++)
 		{
-			for (j = 48; j <= 57; j++)
+			for (k = 48; k <= 57; k++)
 			{
-				if (((k + j) > (c + i) && k >= c) || c < k)
+				for (j = 48; j <= 57; j++)
 				{
-					putchar(c);
-					putchar(i);
-					putchar(k);
-					putchar(j);
+					if (((k + j) > (c + i) && k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(k);
+						putchar(j);
 
-					if (c + i + k + j == 227 && c == 57)
-					{
-						break;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
+						if (c + i + k + j == 227 && c == 57)
+						{
+							break;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}

a42b45a49f2d5cd9ae50675b9571bc0f1c8df258
kassimu21 <sksallah45@gmail.com>
2022-09-09 13:32:41 -0700

    My eighth if else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Description: prints lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

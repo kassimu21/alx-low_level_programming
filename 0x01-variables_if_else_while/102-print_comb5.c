d40646af50998ff6112cd4dc9de9a027309f102a
kassimu21 <sksallah45@gmail.com>
2022-09-09 15:19:53 -0700

    CORRECTED IF ELSE

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,6 +18,7 @@ int main(void)
 				for (j = 48; j <= 57; j++)
 				{
 					if (((k + j) > (c + i) && k >= c) || c < k)
+					
 					{
 						putchar(c);
 						putchar(i);
@@ -25,6 +26,7 @@ int main(void)
 						putchar(j);
 
 						if (c + i + k + j == 227 && c == 57)
+						
 						{
 							break;
 						}

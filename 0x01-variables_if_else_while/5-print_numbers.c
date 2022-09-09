56921db0a7c017d66f23acb3c480cdccc02f50bb
kassimu21 <sksallah45@gmail.com>
2022-09-09 12:38:00 -0700

    My sixth if else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints allsingle digit numbers of base 10 from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0)
+}

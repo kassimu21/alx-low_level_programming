8514edb63da178df7160e3943d2dc356e5ee3cee
kassimu21 <sksallah45@gmail.com>
2022-09-13 13:54:17 -0700

    Final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * _putchar - Display to std out
- * c: parameter to compare
- * Return: 1 or 0
+ * print_alphabet - funtion to print abc
+ * 
+ * Return: 0
  */
- int _putchar(char c);
- /**
- * int - a data type to specify the kind of value that is used in the function
- * _putchar - the "_" before the putchar is used when declaring a function
- * (char *c) - is the arguments passed
- */
-
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char c;
 
-	while(letter <= 'z')
+	for (c = 'a'; c<= 'z'; c++)
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(c);
 	}
-
 	_putchar('\n');
 }

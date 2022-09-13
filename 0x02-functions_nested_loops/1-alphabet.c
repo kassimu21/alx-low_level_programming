c5a00ac7d0cb281e622ffbb3423bdbf73f72c5d5
kassimu21 <sksallah45@gmail.com>
2022-09-13 07:02:09 -0700

    Add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	putchar(a);
-	putchar('\n');
+	_putchar(a);
+	_putchar('\n');
 }

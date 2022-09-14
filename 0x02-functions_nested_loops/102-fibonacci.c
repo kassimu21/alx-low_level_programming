661657bdbbc300319d4e7bda4184c93f93919473
kassimu21 <sksallah45@gmail.com>
2022-09-14 09:46:20 -0700

    School so cool

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+/**
+ * main - main block
+ * Description - program that prints first 50 Fibonacci numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int count = 2;
+	long int i =1, j = 2;
+	long int k;
+	printf("%lu, ", i);
+	while (count <= 50)
+	{
+	if (count == 50)
+	{
+	printf("%lu\n", j);
+	}
+	else
+	{
+	printf("%lu, ", j);
+	}
+	k = j;
+	j + = i;
+	i = k;
+	count++;
+	}
+	return (0);
+}

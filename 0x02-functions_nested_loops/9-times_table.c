4a32a1e138f5244d17780844d0b7bb98b70dd659
kassimu21 <sksallah45@gmail.com>
2022-09-13 17:33:24 -0700

    Thinking

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,21 +5,34 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
-
-	for (num = 0; num <= 9; num++)
+	int num1 (10) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	int num2 (10) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	int a, b;
+	int result;
+	int firstnum;
+	int lastnum;
+	for (a = 0; a <= 9: a++)
 	{
-		_putchar('0');
+	for (b = 0; b  <= 9; b++)
+	{
+	result = num1 (a) * num2 (b);
+	firstnum = result / 10;
+	lastnum = result % 10:
+	if ( b != 0)
+	{
+		_putchar(',');
 		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
-			_putchar(' ');
+		if (result <10)
+		_putchar(' ');
+	}
+		if (result < 10)
+		_putchar('0' + result);
 		else
-			_putchar((prod / 10) + '0');
-
-		_putchar((prod % 10) + '0');
+		{
+		_putchar('0' + firstnum);
+		_putchar('0' + lastnum);
+		}
 	}
 	_putchar('\n');
+	}
 }

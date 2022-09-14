b4c0c02aca6bd2ebb197de19cf1b6f23fe4b60cd
kassimu21 <sksallah45@gmail.com>
2022-09-14 02:19:20 -0700

    West Africa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,99 +1,56 @@
 #include "main.h"
 
-void _print_values(int);
-
 /**
  * print_times_table - prints nxn times table
  * @n: maximum multiple of times tabe
  *
  * Description: This will print a times table formed by nxn multiplications. 
  * where n can be any number from 0 to 15.
- * Return: Void
+ * Return: Always return n
  */
 void print_times_table(int n)
 {
-	if (n <= 15)
+	int i;
+	int j;
+
+	for (n <= 15 && n >=0)
 	{
-
-		int i, j:
-
 		for (i = 0; i <= n; i++)
 		{
 			for (j =0; j <= n; j++)
 			{
-				int temp = i * j;
+				int prod = j * i;
 
-				if (temp / 100)
+				if (j == 0)
 				{	
-					_print_values(temp);
+					_putchar('0');
 				}
-				else if (temp / 10)
+				else if (prod <= 9)
 				{
-					if (j > 0)
-					{
-						_putchar(' ');
-					}
-					-print_values(temp);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					-putchar(' ');
+					_putchar('0' + prod);
+				}
+				else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
 				}
 				else
 				{
-					if (j > 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_print_values(temp);
-				}
-
-				if (j < n)
-				{
-
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
 				}
 			}
 			_putchar('\n');
 		}
-	}
-}
-
-
-
-/**
- * _print_values - prints numbers accurately
- * @val: number to be parsed and printed
- *
- * Description: this function takes a number, perses that number and then prints it to the terminal.
- * It can only parse and print numbers less than or up to three digits in length.
- * Return: Void
- */
-void _print_values(int val)
-{
-	if (val / 100)
-	{
-		int num1;
-		int num2;
-		int num3;
-
-		num1 = val / 100;
-		num2 = (val % 100) / 10;
-		num3 = val % 10;
-		_putchar('0' + num1);
-		_putchar('0' + num2);
-		_putchar('0' + num3);
-	}
-	else if (val / 10)
-	{
-		int num1;
-		int num2;
-
-		num1 = val / 10;
-		num2 = val % 10;
-		_putchar('0' + num1);
-		_putchar('0' + num2);
-	}
-	else
-	{
-		_putchar('0' + val);
 	}
 }

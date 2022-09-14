85bcc30cd8b8ebe7eddab3042f9f8fa169161ef9
kassimu21 <sksallah45@gmail.com>
2022-09-14 11:11:31 -0700

    Spike

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,24 +4,19 @@
  *
  * Return: nothing
  */
+
 int main(void)
 {
-int counter = 0;
+	int a = 0, b =1, c = 0;
+	int sum = 0;
 
-float a = 1;
-float b = a;
-float c = a + b;
-
-while (c < 4000000)
-{
-if (c % 2 == 0)
-{
-counter += c;
-}
-a = b;
-b = c;
-c = a + b;
-}
-printf("%d\n", counter);
-return (0);
+	while (c < 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (next % 2 == 0)
+			sum += c;
+	printf("%i\n", sum);
+	return (0);
 }

bd47517870bb402281778a3dadd1f99a16f93713
kassimu21 <sksallah45@gmail.com>
2022-09-14 14:08:42 -0700

    My first debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+
+/**
+
+* main - tests function that prints if integer is positive or negative
+
+* Return: 0
+
+*/
+
+
+
+int main(void)
+
+{
+
+        int i;
+
+
+
+        i = 98;
+
+        positive_or_negative(i);
+
+
+
+        return (0);
+
+}
+^c
+
+[
+
+
+cdc/root
+cd /root
+^C
+:WQ
+:wq
+:wq

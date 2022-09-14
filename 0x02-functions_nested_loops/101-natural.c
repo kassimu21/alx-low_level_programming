7a00b3b7469917d4e8dac3865174ed94141870d2
kassimu21 <sksallah45@gmail.com>
2022-09-14 09:19:36 -0700

    My fourteenth nested loop

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - sums integers in multiples of integers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int start_num, end_num, total;
+
+	end_num = 1024;
+	total = 0;
+	for (start_num = 0; start_num < end_num; start_num++)
+	{
+		if ((start_num % 3 == 0) || (start_num % 5 == 0))
+		{
+			total = total + start_num;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d", total);
+	printf("\n");
+	return (0);
+}

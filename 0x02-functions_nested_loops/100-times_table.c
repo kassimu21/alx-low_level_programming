ada772e89d6908e6b568b40c2eec4fb34ba7c34c
kassimu21 <sksallah45@gmail.com>
2022-09-14 07:08:56 -0700

    Finished at last

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,16 +32,15 @@ void print_times_table(int n)
 				_putchar(32);
 				_putchar((c / 10) + 48);
 				_putchar((c % 10) + 48);
-
 			}
 			else
 			{
 				_putchar(((c / 100) % 10) + 48);
-				_putchar((c / 10) % 10 + 48);
+				_putchar(((c / 10) % 10) + 48);
 				_putchar((c % 10) + 48);
 			}
+			}
 			_putchar('\n');
-		}
 		}
 	}
 }

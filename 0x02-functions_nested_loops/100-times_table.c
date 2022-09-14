5c595f8b45adbfe331e25feb022c14db8c56ac35
kassimu21 <sksallah45@gmail.com>
2022-09-14 03:35:18 -0700

    Moving

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,44 +10,34 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-
-	for (i = 0; i <= n; i++)
+	int prod, mult, num;
+	
+	if (n <= 15 && n >= 0)
 	{
-		for (j =0; j <= n; j++)
+		for (num = 0; num <= n; ++num)
 		{
-			int prod = j * i;
-
-			if (j == 0)
-			{	
-				_putchar('0');
-			}
-			else if (prod <= 9)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + prod);
+			
+				prod = num * mult;
+				
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+				
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+					_putchar((prod % 10) + 48);
 			}
-			else if (prod > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (prod / 100));
-				_putchar('0' + ((prod / 10) % 10));
-				_putchar('0' + (prod % 10));
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (prod / 10));
-				_putchar('0' + (prod % 10));
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

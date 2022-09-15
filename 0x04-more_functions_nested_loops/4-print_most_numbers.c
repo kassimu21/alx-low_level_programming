bd440aa5e5327fb7ab9032bd5cb4b968e00fa6f8
kassimu21 <sksallah45@gmail.com>
2022-09-15 09:20:45 -0700

    Hads

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,19 +7,16 @@
  * terminal except the numbers 2 and 4.
  * Return: 0
  */
-
 void print_most_numbers(void)
 {
-	char i = 0;
+	int i;
 	
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar('0' + i);
-		}
-		
-		i++;
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 

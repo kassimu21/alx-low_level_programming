2b174ce3b03ca2d9f5e723dacfb8a8217854e7fa
kassimu21 <sksallah45@gmail.com>
2022-09-15 14:09:28 -0700

    My rest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the numbers from 1 to 100
@@ -18,16 +17,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (i == 100)
-		printf("%s", b);
-	else if ((i % 3 == 0) && (i % 5 == 0))
-		printf("%s", fb);
-	else if (i % 3 == 0)
-		printf("%s", f);
-	else if (i % 5 == 0)
-		printf("%s", b);
-	else
-		printf("%d", i);
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", fb);
+		else if (i % 3 == 0)
+			printf("%s", f);
+		else if (i % 5 == 0)
+			printf("%s", b);
+		else
+			printf("%d", i);
 	}
 	printf("\n");
 	return (0);

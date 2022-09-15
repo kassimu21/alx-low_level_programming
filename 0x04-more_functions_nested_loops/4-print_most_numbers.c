a98072370060cc43fcff26c46d12fcc8bb3a1f2d
kassimu21 <sksallah45@gmail.com>
2022-09-15 09:24:24 -0700

    Best

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,6 +12,7 @@ void print_most_numbers(void)
 	int i;
 	
 	for (i = 0; i <= 9; i++)
+
 	{
 		if ((i == 2) || (i == 4))
 			continue;
@@ -19,5 +20,4 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 	}
 	_putchar('\n');
-
 }

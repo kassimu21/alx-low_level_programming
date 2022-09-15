688379221170171179c735560e58e56d26a673cb
kassimu21 <sksallah45@gmail.com>
2022-09-15 15:40:01 -0700

    No more crying

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,23 +5,24 @@
  * @size: size of the triangle
  * Return: triangle of '#'s
  */
+
 void print_triangle(int size)
 {
-	int sp, ro, tr;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (ro = 0; ro <= (size - 1); ro++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			for (sp = 0; sp < (size - 1) - ro; sp++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (tr = 0; tr <= ro; tr++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}

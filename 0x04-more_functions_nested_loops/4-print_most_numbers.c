d73c0a2c096cfa68ae2090b9f4630fb2d6dd4cd8
kassimu21 <sksallah45@gmail.com>
2022-09-15 09:35:47 -0700

    Zobo

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the most numbers
- * 
- * Description: prints out numbers from 0 to 9 to
- * terminal except the numbers 2 and 4.
- * Return: 0
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
 void print_most_numbers(void)
 {
 	int i;
-	
-	for (i = 0; i <= 9; i++)
 
+	for (i = 0; i <= 9; i++)
 	{
 		if ((i == 2) || (i == 4))
 			continue;

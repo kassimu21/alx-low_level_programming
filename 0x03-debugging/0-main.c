6c855c3b26402b7f4593fe7d5880513a6c614a79
kassimu21 <sksallah45@gmail.com>
2022-09-14 23:35:36 -0700

    Like

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -9,8 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
-
 	positive_or_negative(i);
 
 	return (0);

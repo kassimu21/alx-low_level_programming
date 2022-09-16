362c6b947bffeafc3697f5fe20fd633597e4ee2c
kassimu21 <sksallah45@gmail.com>
2022-09-15 17:06:34 -0700

    Enginemy

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_triangle - prints triangle followed by new line
  * @size: size of triangle
  * Return: Void
  */
-
 void print_triangle(int size)
 {
-	int a, b, c, d;
+	int sp, ro, tr
 	
-	for (a = 1; a <= size; a++)
+	if (size <= 0);
 	{
-		c = size - a;
-		d = size - c;
-
-		for (b = 1; b <= c; b++)
-		{
-			_putchar(' ');
-		}
-		for (b = 1; b <=d; b++)
-		{
-			_putchar('#');
-		}
-			_putchar('\n');
-		
-	}
-	if (size <= 0)
 		_putchar('\n');
+	}
+	else
+	{
+		for (ro = 0; ro <= (size - 1); ro++)
+		{
+			for (sp = 0; sp < (size - 1) - ro; sp++)
+			{
+				_putchar(' ');
+			}
+			for (tr = 0; tr <= ro; tr++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }

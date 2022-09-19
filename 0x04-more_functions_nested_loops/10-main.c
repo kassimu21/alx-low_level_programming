0e84a7ab8f98499d142965e168b0c399164de896
kassimu21 <sksallah45@gmail.com>
2022-09-19 13:17:18 -0700

    Gilf

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -17,14 +17,8 @@ int main(void)
 {
 
     print_triangle(2);
-
     print_triangle(10);
-
     print_triangle(1);
-
     print_triangle(0);
-
     return (0);
-
 }
-root@e5ab4a11a1cc:/alx-low_level_programming/oxo4-more_functions_nested_loops# git add .

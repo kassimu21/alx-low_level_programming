081d4de7e9c7200c066ff92e9600329a146f0479
kassimu21 <sksallah45@gmail.com>
2022-09-22 02:17:24 -0700

    My first pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,31 @@
 #include "main.h"
+
 /**
- * *_strcat - concatenates the string pointer to by @src.
- * null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strcat - concatenated two strings
+ * @dest: string to append to
+ * @src: string to to
+ * @src: sting to add
  *
- * Return: A pointer to the destination string @dest.
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-	int indesx = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
+	j = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }

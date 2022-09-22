4b9d60c19fb16fff96331ffeb60d1b250e353875
kassimu21 <sksallah45@gmail.com>
2022-09-22 09:58:11 -0700

    Human

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,26 +6,22 @@
  * @src: source string
  * @n: number of bytes of str to concatenate
  * 
- * Return: a pointer to the resulting string dest
+ * * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j = 0;
 	
-	i = 0;
-	j = 0;
-	
-	while (dest[i] != '\0')
+	while (*(dest + i) != '\0')
 		i++;
 	
-	while (src[j] != '\0' && j < n)
+	while (*(src + j) != '\0' && j < n)
 	{
-		dest[i] = src[j];
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
-	
-	dest[i] = '\0';
+	*(dest + i) = '\0';
 	
 	return (dest);
 }

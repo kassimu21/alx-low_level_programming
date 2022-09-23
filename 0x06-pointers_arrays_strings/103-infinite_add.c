a2eb41aa50276363213881b46d2d2c4c091d6034
kassimu21 <sksallah45@gmail.com>
2022-09-23 09:36:59 -0700

    My eleventh pointer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,26 +22,26 @@ char *infinite_add(char *n1, char *n2, char *r, int int sizer_r)
 		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	n = m;
-	if (i >= 0)
-		n += n1[i] - '0';
-	if (j >= 0)
-		n += n2[j] - '0';
-	if (i < 0 && j < 0 && n == 0)
-	{
-		break;
-	}
-	m = n / 10;
-	r[k] = n % 10 + '0';
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-	for (k -= 1, | = 0; | < k; k--, | ++)
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
 		m = r[k];
-		r[k] = r[|];
-		r[|] = m;
+		r[k] = r[l];
+		r[l] = m;
 	}
 	return (r);
 }

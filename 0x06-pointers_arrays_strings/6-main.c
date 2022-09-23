5527986098fefc71aa7c1d232c78dc8e62651200
kassimu21 <sksallah45@gmail.com>
2022-09-23 06:39:27 -0700

    6main.c

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[] = "
+Expect the best. prepare foor the worst. Capitalize on what comes. \n
+hello world! hellow-world 0123456hello world\thello world.hello world\n";
+	char *p;
+
+	p = cap_string(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}

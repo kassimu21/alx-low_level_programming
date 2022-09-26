c906428193a7352f84e9962303900e99532e5d24
kassimu21 <sksallah45@gmail.com>
2022-09-26 09:31:53 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,15 +3,18 @@
  * _strchr - locate a character in a string
  * @s:string
  * @c:char
- * Return pointer to char*
+ * Retur
+ * n pointer to char*
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-for (; s[i] >= '\0'; i++)
-{
-if (s[i] == c)
-return (&s[i]);
-}
-return (0);
+	int i;
+
+	for (i = 0;  s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		return (s + i);
+	}
+
+	return ('\0');
 }

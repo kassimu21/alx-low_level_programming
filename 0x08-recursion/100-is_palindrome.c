c59066d962c1f4c17a9aa7fd2b2663a4b5b8a2f8
kassimu21 <sksallah45@gmail.com>
2022-09-28 05:50:08 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,11 +12,11 @@ int _strlen_recursion(char *s)
 		return (1 + _strlen_recursion(s + 1));
 }
 /**
- * comparator _ compares each character of the string.
+ * comparator - compares each character of the string.
  * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
- * Return: string.
+ * Return: .
  */
 int comparator(char *s, int n1, int n2)
 {

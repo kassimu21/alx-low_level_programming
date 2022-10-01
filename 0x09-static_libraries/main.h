fffb89a4b3a46598f7b2db87f61e17eb6d0b2cde
kassimu21 <sksallah45@gmail.com>
2022-10-01 14:59:26 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-/*a list that was given from the task 0 */
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -20,7 +18,7 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
+unsigned int_strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 

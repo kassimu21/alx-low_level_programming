6c7e5898c09f84b47cf7ec69f13fd9ea9782cd41
kassimu21 <sksallah45@gmail.com>
2022-10-05 01:41:04 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,8 +19,8 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int_strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */

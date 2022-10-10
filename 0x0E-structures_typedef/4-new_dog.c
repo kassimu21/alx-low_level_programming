5ba71d015cecb81e195aa71e25a4df900a76c62f
kassimu21 <sksallah45@gmail.com>
2022-10-10 05:00:38 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,7 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 
 	p_dog->name = malloc(lname + 1);
-	p-dog->owner = malloc(lowner + 1);
+	p_dog->owner = malloc(lowner + 1);
 
 	if (!(p_dog->name) || !(p_dog->owner))
 	{

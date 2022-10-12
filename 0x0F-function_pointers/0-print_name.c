293908578173d1eb7d05befa022f6b45dff9d20d
kassimu21 <sksallah45@gmail.com>
2022-10-12 13:06:21 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - prints a name as is
  * @name: name of the person
- * @of: is pointer
+ * @f: is pointer
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))

9c39edcca13aa295e05e5a040fe2cf01f50a0229
root <sksallah45@gmail.com>
2024-04-09 05:54:45 -0700

    updated

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -1,24 +1,15 @@
-#include "main.h"
-
-
+#include "holberton.h"
 
 /**
-
- * main - check the code
-
- *
-
- * Return: Always 0.
-
- */
-
+ *  * main - check the code for ALX School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
-
 {
-
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
+	    print_triangle(2);
+	        print_triangle(10);
+		    print_triangle(1);
+		        print_triangle(0);
+			    return (0);
 }
